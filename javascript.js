cf5efd3c24fc69d6ec31a8c307ba41d60c63f0b3
ShubhankerKumar <157530834+ShubhankerKumar@users.noreply.github.com>
2024-06-07 15:26:39 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const dot = document.querySelector('.dot');
+const nav = document.querySelector('.nav-links');
+
+dot.addEventListener('click',  () => {
+    nav.classList.toggle('nav-action')
+});
+
+
+function send() {
+    let num = document.getElementById("number").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+
+    let box = "Number: " + num + "<br/> Email: " + email + "<br/> Message: " + message;
+    console.log(box)
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "bansalshanu704@gmail.com",
+        Password: "A04ED1E975D3ED6252D139234D13D26E19D9",
+        To: 'shubhanker.sk2001@gmail.com',
+        From: "bansalshanu704@gmail.com",
+        Subject: "This is the subject",
+        Body: box
+    }).then(
+        message => alert(message)
+    );
+
+}
